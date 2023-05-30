7a1f166c3a2c293e9ac085f21ac6280171f5508f
puarns <puarns@gmail.com>
2023-05-30 15:40:38 +0700

    add event

---
+++ common/event.go
@@ -0,0 +1,54 @@
+package common
+
+import (
+	"bufio"
+	"sync"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Channel struct {
+	Name    string
+	Clients map[*bufio.Writer]struct{}
+	sync.RWMutex
+}
+
+type EventServer struct {
+	Channels map[string]*Channel
+	sync.RWMutex
+}
+
+func NewEventServer() *EventServer {
+	return &EventServer{
+		Channels: make(map[string]*Channel),
+	}
+}
+
+func (s *EventServer) GetOrCreateChannel(name string) *Channel {
+	s.RLock()
+	channel, ok := s.Channels[name]
+	s.RUnlock()
+	if !ok {
+		s.Lock()
+		channel = &Channel{
+			Name:    name,
+			Clients: make(map[*bufio.Writer]struct{}),
+		}
+		s.Channels[name] = channel
+		s.Unlock()
+	}
+	return channel
+}
+
+func (s *EventServer) RemoveClientFromChannel(channel *Channel, client *bufio.Writer) {
+	channel.Lock()
+	delete(channel.Clients, client)
+	channel.Unlock()
+}
+
+func PrepareHeaderForSSE(c *fiber.Ctx) {
+	c.Set("Content-Type", "text/event-stream")
+	c.Set("Cache-Control", "no-cache")
+	c.Set("Connection", "keep-alive")
+	c.Set("Transfer-Encoding", "chunked")
+}
