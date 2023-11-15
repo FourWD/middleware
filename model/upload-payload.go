c880d4034e7670335b8604f909b2c0cf45b0ea3c
Pattanapong <pattanapong.amu@gmail.com>
2023-11-15 11:11:22 +0700

    Update upload-payload.go
    fix context payload

--- model/upload-payload.go
+++ model/upload-payload.go
@@ -1,9 +1,9 @@
 package model
 
 type UploadPayload struct {
-	AppID      string `query:"app_id" json:"app_id"`
-	BucketName string `query:"bucket_name" json:"bucket_name"`
-	Path       string `query:"path" json:"path"`
-	Filename   string `query:"filename" json:"filename"`
-	FileBase64 string `query:"file_base_64" json:"file_base_64"`
+	AppID      string `json:"app_id" query:"app_id"`
+	BucketName string `json:"bucket_name" query:"bucket_name"`
+	Path       string `json:"path" query:"path"`
+	Filename   string `json:"filename" query:"filename"`
+	FileBase64 string `json:"file_base_64" query:"file_base_64"`
 }
