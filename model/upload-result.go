6f12fd8257a4f97563295cded05ff856a06c47e7
jamenrtmp <narongritjamenrtmp@gmail.com>
2023-11-21 10:56:52 +0700

    update model

--- model/upload-result.go
+++ model/upload-result.go
@@ -1,10 +1,11 @@
 package model
 
 type UploadResult struct {
-	ID        string `json:"id"`
-	Cdn       string `json:"cdn"`
-	FileName  string `json:"file_name"`
-	Extension string `json:"extension"`
-	Path      string `json:"path"`
-	FullPath  string `json:"full_path"`
+	ID         string `json:"id"`
+	BucketName string `json:"bucket_name"`
+	Cdn        string `json:"cdn"`
+	FileName   string `json:"file_name"`
+	Extension  string `json:"extension"`
+	Path       string `json:"path"`
+	FullPath   string `json:"full_path"`
 }
