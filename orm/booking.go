81efa907c76521936a410427f9e1ee487c04e514
jamenrtmp <narongritjamenrtmp@gmail.com>
2023-11-29 15:33:30 +0700

    update commit

--- orm/booking.go
+++ orm/booking.go
@@ -7,7 +7,8 @@ import "github.com/FourWD/middleware/model"
 type Booking struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36)"` //
 	model.GormModel
-	BookingNo string `json:"booking_no" query:"booking_no" gorm:"type:varchar(20)"` //หมายเลขรายการ
+	BookingNo       string `json:"booking_no" query:"booking_no" gorm:"type:varchar(20)"` //หมายเลขรายการ
+	BookingStatusID string `json:"booking_status_id" query:"booking_status_id" gorm:"type:varchar(2)"`
 
 	PrefixID  string `json:"prefix_id" query:"prefix_id" gorm:"type:varchar(2)"`
 	FirstName string `json:"first_name" query:"name" gorm:"type:varchar(255)"`
