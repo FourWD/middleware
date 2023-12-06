91bf6d1ed11f93f41f2b43a2333808fbc48ef234
winra159@gmail.com <winra159@gmail.com>
2023-12-06 19:22:39 +0700

    change is boolean type

--- orm/booking.go
+++ orm/booking.go
@@ -20,8 +20,8 @@ type Booking struct {
 	VehicleModelID    string `json:"vehicle_model_id" query:"vehicle_model_id" gorm:"type:varchar(36)"`
 	VehicleSubModelID string `json:"vehicle_sub_model_id" query:"vehicle_sub_model_id" gorm:"type:varchar(36)"`
 	VehicleColorID    string `json:"vehicle_color_id" query:"vehicle_color_id" gorm:"type:varchar(36)"`
-	IsPaid            string `json:"is_paid" query:"is_paid" gorm:"type:tinyint(1)"`
-	IsCancle          string `json:"is_cancle" query:"is_cancle" gorm:"type:tinyint(1)"`
+	IsPaid            bool   `json:"is_paid" query:"is_paid" gorm:"type:tinyint(1)"`
+	IsCancle          bool   `json:"is_cancle" query:"is_cancle" gorm:"type:tinyint(1)"`
 	Remark            string `json:"remark" query:"remark" gorm:"type:text"`
 	EmpolyeeID        string `json:"empolyee_id" query:"empolyee_id" gorm:"type:varchar(36)"`
 
