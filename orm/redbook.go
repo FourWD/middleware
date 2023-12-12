36c4a4e400e9a4ab583842203b741db670df122d
puarns <puarns@gmail.com>
2023-12-12 11:33:51 +0700

    no message

--- orm/redbook.go
+++ orm/redbook.go
@@ -6,7 +6,7 @@ type Redbook struct {
 	AuctionID     string    `json:"auction_id" query:"auction_id" gorm:"type:varchar(2);"`
 	ChassisNo     string    `json:"chassis_no" query:"chassis_no" gorm:"type:varchar(20)"`
 	CRP           int       `json:"crp" query:"crp" gorm:"type:int"`
-	CRPreVAT      int       `json:"crp_pre_vat" query:"crp_pre_vat" gorm:"type:int"`
+	CRPPreVat     int       `json:"crp_pre_vat" query:"crp_pre_vat" gorm:"type:int"`
 	License       string    `json:"license" query:"license" gorm:"type:varchar(10)"`
 	RedbookCode   string    `json:"redbook_code" query:"license" gorm:"type:varchar(20)"`
 	RedbookDate   time.Time `json:"redbook_date" query:"redbook_date"`
