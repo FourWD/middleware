fa8c7a1ec3f878c94d506cc320bb52eea25afc69
puarns <puarns@gmail.com>
2023-12-12 11:20:45 +0700

    no message

--- orm/redbook.go
+++ orm/redbook.go
@@ -9,7 +9,7 @@ type Redbook struct {
 	CRPreVAT      int       `json:"crp_pre_vat" query:"crp_pre_vat" gorm:"type:int"`
 	License       string    `json:"license" query:"license" gorm:"type:varchar(10)"`
 	RedbookCode   string    `json:"redbook_code" query:"license" gorm:"type:varchar(20)"`
-	RedbookDate   string    `json:"redbook_date" query:"redbook_date" gorm:"type:varchar(20)"`
+	RedbookDate   time.Time `json:"redbook_date" query:"redbook_date"`
 	RedbookPreVat int       `json:"redbook_pre_vat" query:"redbook_pre_vat" gorm:"type:int"`
 	Remark        string    `json:"remark" query:"remark" gorm:"type:varchar(20)"`
 	CreatedAt     time.Time `json:"created_at" query:"created_at" gorm:"<-:create"`
