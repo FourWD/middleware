d0aa0e871e42fd2e83826ed87e021b1d16071204
puarns <puarns@gmail.com>
2023-12-20 07:11:04 +0700

    var DATE_FORMAT_NANO = "2006-01-02 15:04:05.99999" var DATE_FORMAT_SECOND = "2006-01-02 15:04:05" var DATE_FORMAT_MINUTE = "2006-01-02 15:04" var DATE_FORMAT_DAY = "2006-01-02"

--- common/time.go
+++ common/time.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+var DATE_FORMAT_NANO = "2006-01-02 15:04:05.99999"
+var DATE_FORMAT_SECOND = "2006-01-02 15:04:05"
+var DATE_FORMAT_MINUTE = "2006-01-02 15:04"
+var DATE_FORMAT_DAY = "2006-01-02"
+
 func SetThailandTimezone() {
 	location, err := time.LoadLocation("Asia/Bangkok")
 	if err != nil {
