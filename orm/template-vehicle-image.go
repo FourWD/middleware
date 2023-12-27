e11d9f7220c94541dd6a6d4c1900b980b738f01b
p-amu <99467582+p-amu@users.noreply.github.com>
2023-12-27 21:26:06 +0700

    Update template-vehicle-image.go
    add is_active fields

--- orm/template-vehicle-image.go
+++ orm/template-vehicle-image.go
@@ -11,6 +11,7 @@ type TemplateVehicleImage struct {
 	VehicleImageGroupID string  `json:"vehicle_type_id" query:"vehicle_type_id" gorm:"type:varchar(36)"`
 	Name                string  `json:"name" query:"name" gorm:"type:varchar(255)"`
 	RowOrder            float32 `json:"row_order" query:"row_order" gorm:"type:decimal(5,2)"`
+	IsActive            bool    `json:"is_active" query:"is_active" gorm:"type:bool"`
 }
 
 // รูป ด้านซ้าย ด้านขวา ของภายหน้า
