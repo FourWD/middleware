d283e08e0fb38da8de08bcd2fd4bb6b56445908a
p-amu <99467582+p-amu@users.noreply.github.com>
2023-12-28 22:34:11 +0700

    Update gorm.go
    update gorm firestore

--- model/gorm.go
+++ model/gorm.go
@@ -7,15 +7,15 @@ import (
 )
 
 type GormModel struct {
-	CreatedAt time.Time      `json:"created_at" query:"created_at" gorm:"<-:create"`
-	UpdatedAt time.Time      `json:"updated_at" query:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" query:"deleted_at" gorm:"index"`
+	CreatedAt time.Time      `json:"created_at" query:"created_at" firestore:"created_at" gorm:"<-:create"`
+	UpdatedAt time.Time      `json:"updated_at" query:"updated_at" firestore:"updated_at" `
+	DeletedAt gorm.DeletedAt `json:"deleted_at" query:"deleted_at" firestore:"deleted_at" gorm:"index"`
 
-	CreatedBy string `json:"created_by" query:"created_by" gorm:"type:varchar(36)"`
-	UpdatedBy string `json:"updated_by" query:"updated_by" gorm:"type:varchar(36)"`
-	DeletedBy string `json:"deleted_by" query:"deleted_by" gorm:"type:varchar(36)"`
+	CreatedBy string `json:"created_by" query:"created_by" firestore:"created_by" gorm:"type:varchar(36)"`
+	UpdatedBy string `json:"updated_by" query:"updated_by" firestore:"updated_by" gorm:"type:varchar(36)"`
+	DeletedBy string `json:"deleted_by" query:"deleted_by" firestore:"deleted_by" gorm:"type:varchar(36)"`
 }
 
 type GormRowOrder struct {
-	RowOrder int `json:"row_order" query:"row_order" gorm:"type:int(4);"`
+	RowOrder int `json:"row_order" query:"row_order" firestore:"row_order" gorm:"type:int(4);"`
 }
