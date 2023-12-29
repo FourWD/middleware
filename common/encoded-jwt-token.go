a229c75729f74758782e1454a35b98403846b16b
puarns <puarns@gmail.com>
2023-12-29 13:23:50 +0700

    add GenJwtToken function

--- common/encoded-jwt-token.go
+++ common/encoded-jwt-token.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v2"
 	"github.com/golang-jwt/jwt"
 	"github.com/spf13/viper"
 )
@@ -21,7 +23,6 @@ func EncodedJwtToken(c *fiber.Ctx, res string) (string, error) {
 		return "", errors.New("token = nil ")
 
 	}
-	//secretKeyToken := []byte(os.Getenv("secretKey"))
 	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
 
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
@@ -41,3 +42,49 @@ func EncodedJwtToken(c *fiber.Ctx, res string) (string, error) {
 
 	return "", err
 }
+
+func GenJwtToken(data map[string]interface{}) (string, error) {
+	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
+	// Add additional claims
+	data["expire_date"] = time.Now().Add(time.Minute * 15).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(data))
+	tokenString, err := token.SignedString(secretKeyToken)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func AuthorizationRequired() fiber.Handler {
+	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
+	return jwtware.New(jwtware.Config{
+		SigningMethod: "HS256",
+		SigningKey:    []byte(secretKeyToken),
+		SuccessHandler: func(c *fiber.Ctx) error {
+			return c.Next()
+		},
+		ErrorHandler: func(c *fiber.Ctx, e error) error {
+			return fiber.ErrUnauthorized
+		},
+	})
+}
+
+/*
+func main() {
+	data := map[string]interface{}{
+		"user_id":     123,
+		"user_role":   "admin",
+		"first_name":  "John",
+		"last_name":   "Doe",
+	}
+
+	token, err := GenJwtToken(data)
+	if err != nil {
+		panic("Failed to generate JWT token")
+	}
+
+	// Use the generated token as needed
+	println(token)
+}*/
