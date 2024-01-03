8bdc4abf19a25dc4e26b26b24f4ed8db292751b2
jamenrtmp <narongritjamenrtmp@gmail.com>
2024-01-03 19:55:54 +0700

    update template

--- orm/template-vehicle-image-group.go
+++ orm/template-vehicle-image-group.go
@@ -8,7 +8,7 @@ type TemplateVehicleImageGroup struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
-	Name     string  `json:"name" query:"name" gorm:"type:varchar(20)"`
+	Name     string  `json:"name" query:"name" gorm:"type:varchar(100)"`
 	RowOrder float32 `json:"row_order" query:"row_order" gorm:"type:decimal(5,2)"`
 }
 
