50dff311049df00d261d988756af2b19f2d79d1d
jamenrtmp <narongritjamenrtmp@gmail.com>
2024-01-12 17:03:35 +0700

    update user-type
    add name_en for gen-code

--- orm/user-type.go
+++ orm/user-type.go
@@ -8,7 +8,9 @@ type UserType struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key"`
 	model.GormModel
 
-	Name string `json:"name" query:"name" gorm:"type:varchar(20)"`
+	Name   string `json:"name" query:"name" gorm:"type:varchar(20)"`
+	NameEn string `json:"name_en" query:"name_en" gorm:"type:varchar(2)"`
+
 	model.GormRowOrder
 }
 
