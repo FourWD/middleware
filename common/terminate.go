82b88f25122be45ad9400fabf10ce590e728094d
puarns <puarns@gmail.com>
2024-01-18 07:37:01 +0700

    no message

--- common/terminate.go
+++ common/terminate.go
@@ -3,9 +3,12 @@ package common
 import (
 	"log"
 	"os"
+
+	"github.com/spf13/viper"
 )
 
 func Terminate() {
+	log.Println("Terminate App Version: ", viper.GetString("app_version"))
 	zero := 0
 	i := 1 / zero
 	log.Panic(i)
