bbfbda57923d94237a8677e9e690f680fbbbf799
puarns <puarns@gmail.com>
2024-01-21 13:53:36 +0700

    no message

---
+++ common/jwt.go
@@ -0,0 +1,189 @@
+package common
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/FourWD/middleware/orm"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+)
+
+var (
+	secretKey     = []byte(viper.GetString("secret-key"))
+	refreshSecret = []byte(viper.GetString("refresh-secret-key"))
+)
+
+type JWTClaims struct {
+	UserID   string `json:"user_id"`
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func generateToken(userID string, username string, key []byte, expiresIn time.Duration) (string, error) {
+	claims := JWTClaims{
+		UserID:   userID,
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(expiresIn).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func authenticate(username, password string) (orm.User, bool) {
+	if username == "user" && password == "password" {
+		return orm.User{ID: "1", Username: "user", Password: "password"}, true
+	}
+	return orm.User{}, false
+}
+
+func isExcludedPath(path string) bool {
+	excludedPaths := []string{"login", "logout", "register", "wake-up", "warmup"}
+	for _, p := range excludedPaths {
+		if strings.Contains(path, p) {
+			return true
+		}
+	}
+	return false
+}
+
+func AuthenticationMiddleware(c *fiber.Ctx, excludePath ...[]string) error {
+	if isExcludedPath(c.Path()) {
+		return c.Next()
+	}
+
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.Status(http.StatusUnauthorized).SendString("No token provided")
+	}
+
+	tokenString := authHeader[len("Bearer "):]
+
+	claims := &JWTClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return c.Status(http.StatusUnauthorized).SendString("Invalid token signature")
+		}
+		return c.Status(http.StatusBadRequest).SendString("Bad request")
+	}
+
+	if !token.Valid {
+		return c.Status(http.StatusUnauthorized).SendString("Invalid token")
+	}
+
+	c.Locals("user", claims)
+	return c.Next()
+}
+
+func Login(app *fiber.App) {
+	app.Post("/login", func(c *fiber.Ctx) error {
+		var user orm.User
+		if err := c.BodyParser(&user); err != nil {
+			return err
+		}
+
+		if authenticatedUser, ok := authenticate(user.Username, user.Password); ok {
+			accessToken, err := generateToken(authenticatedUser.ID, authenticatedUser.Username, secretKey, time.Hour*24)
+			if err != nil {
+				return err
+			}
+
+			refreshToken, err := generateToken(authenticatedUser.ID, authenticatedUser.Username, refreshSecret, time.Hour*24*30) // 30 days
+			if err != nil {
+				return err
+			}
+
+			return c.JSON(fiber.Map{"access_token": accessToken, "refresh_token": refreshToken})
+		}
+
+		return c.Status(http.StatusUnauthorized).SendString("Invalid credentials")
+	})
+}
+
+func RefreshToken(c *fiber.Ctx) error {
+	refreshToken := c.FormValue("refresh_token")
+	if refreshToken == "" {
+		return c.Status(http.StatusBadRequest).SendString("Refresh token not provided")
+	}
+
+	claims := &JWTClaims{}
+	token, err := jwt.ParseWithClaims(refreshToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return refreshSecret, nil
+	})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return c.Status(http.StatusUnauthorized).SendString("Invalid refresh token signature")
+		}
+		return c.Status(http.StatusBadRequest).SendString("Bad request")
+	}
+
+	if !token.Valid {
+		return c.Status(http.StatusUnauthorized).SendString("Invalid refresh token")
+	}
+
+	// Refresh token is valid, generate a new access token
+	newAccessToken, err := generateToken(claims.UserID, claims.Username, secretKey, time.Hour*24)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(fiber.Map{"access_token": newAccessToken})
+}
+
+// func main() {
+// 	app := fiber.New()
+
+// 	// Use authentication middleware for all routes except excluded ones
+// 	app.Use(authenticationMiddleware)
+
+// 	// Example login endpoint
+// 	app.Post("/login", func(c *fiber.Ctx) error {
+// 		var user User
+// 		if err := c.BodyParser(&user); err != nil {
+// 			return err
+// 		}
+
+// 		if authenticatedUser, ok := authenticate(user.Username, user.Password); ok {
+// 			accessToken, err := generateToken(authenticatedUser.ID, authenticatedUser.Username, secretKey, time.Hour*24)
+// 			if err != nil {
+// 				return err
+// 			}
+
+// 			refreshToken, err := generateToken(authenticatedUser.ID, authenticatedUser.Username, refreshSecret, time.Hour*24*30) // 30 days
+// 			if err != nil {
+// 				return err
+// 			}
+
+// 			return c.JSON(fiber.Map{"access_token": accessToken, "refresh_token": refreshToken})
+// 		}
+
+// 		return c.Status(http.StatusUnauthorized).SendString("Invalid credentials")
+// 	})
+
+// 	// Example protected endpoint
+// 	app.Get("/protected", func(c *fiber.Ctx) error {
+// 		// Access the user from the authentication middleware
+// 		userClaims := c.Locals("user").(*JWTClaims)
+// 		return c.JSON(fiber.Map{"user_id": userClaims.UserID, "username": userClaims.Username})
+// 	})
+
+// 	// Refresh token endpoint
+// 	app.Post("/refresh", refreshTokenHandler)
+
+// 	app.Listen(":3000")
+// }
--- go.mod
+++ go.mod
@@ -27,6 +27,7 @@ require (
 	github.com/MicahParks/keyfunc v1.9.0 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/eapache/go-resiliency v1.3.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
