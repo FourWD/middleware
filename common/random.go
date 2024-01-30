187bf59d13cdfcc62a9354f05acfaab87978cd14
puarns <puarns@gmail.com>
2024-01-30 12:28:56 +0700

    add randomString

--- common/random.go
+++ common/random.go
@@ -1,17 +1,33 @@
 package common
 
 import (
+	"encoding/base64"
+	"fmt"
 	"math/rand"
 )
 
-var charset = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+// var charset = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
-// n is the length of random string we want to generate
-func RandStr(n int) string {
-	b := make([]byte, n)
-	for i := range b {
-		// randomly select 1 character from given charset
-		b[i] = charset[rand.Intn(len(charset))]
+// // n is the length of random string we want to generate
+// func RandStr(n int) string {
+// 	b := make([]byte, n)
+// 	for i := range b {
+// 		// randomly select 1 character from given charset
+// 		b[i] = charset[rand.Intn(len(charset))]
+// 	}
+// 	return string(b)
+// }
+
+func RandomString(length int) string {
+	if length <= 0 {
+		length = 16
 	}
-	return string(b)
+	randomBytes := make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		fmt.Printf("Failed to generate random bytes: %v\n", err)
+	}
+	// Encode the random bytes as a base64 string.
+	randomString := base64.RawURLEncoding.EncodeToString(randomBytes)
+	return randomString
 }
--- common/string.go
+++ common/string.go
@@ -1,27 +1,11 @@
 package common
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"strconv"
 	"time"
 )
-
-func RandomString(length int) string {
-	if length <= 0 {
-		length = 16
-	}
-	randomBytes := make([]byte, length)
-	_, err := rand.Read(randomBytes)
-	if err != nil {
-		fmt.Printf("Failed to generate random bytes: %v\n", err)
-	}
-	// Encode the random bytes as a base64 string.
-	randomString := base64.RawURLEncoding.EncodeToString(randomBytes)
-	return randomString
-}
 
 func DateString() string {
 	currentTime := time.Now()
