f1733e3e23e0c8dd52c0f3b4c2d3feffeeab8c76
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-02-02 18:02:32 +0700

    update template

--- orm/template-vehicle-image.go
+++ orm/template-vehicle-image.go
@@ -8,7 +8,7 @@ type TemplateVehicleImage struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
-	VehicleImageGroupID string  `json:"vehicle_type_id" query:"vehicle_type_id" gorm:"type:varchar(36)"`
+	VehicleImageGroupID string  `json:"vehicle_image_group_id" query:"vehicle_image_group_id" gorm:"type:varchar(36)"`
 	Name                string  `json:"name" query:"name" gorm:"type:varchar(256)"`
 	RowOrder            float32 `json:"row_order" query:"row_order" gorm:"type:decimal(5,2)"`
 	IsActive            bool    `json:"is_active" query:"is_active" gorm:"type:bool"`
