fd5129595ae4cefae103a2a25159f9d413e29991
p-amu <99467582+p-amu@users.noreply.github.com>
2024-02-19 11:15:36 +0700

    consent type add
    add id

--- orm/consent.go
+++ orm/consent.go
@@ -6,6 +6,8 @@ type Consent struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
+	ConsentTypeID string `json:"consent_type_id" query:"consent_type_id" gorm:"type:varchar(36)"`
+
 	ConsentVersion string `json:"consent_version" query:"consent_version" gorm:"type:varchar(3)"`
 	Detail         string `json:"detail" query:"detail" gorm:"type:varchar(500)"`
 }
