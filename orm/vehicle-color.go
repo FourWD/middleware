c5fd7d6a594246f85f0aa247c267f48569ac6433
suradevv <opapayao1@hotmail.com>
2024-02-23 12:28:41 +0700

    update varchar in vehicle_color: name

--- orm/vehicle-color.go
+++ orm/vehicle-color.go
@@ -8,6 +8,6 @@ type VehicleColor struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
-	Name string `json:"name" query:"name" gorm:"type:varchar(20)"`
+	Name string `json:"name" query:"name" gorm:"type:varchar(36)"`
 	model.GormRowOrder
 }
