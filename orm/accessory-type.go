f7517cf9bef2941845fbfde90d2ce3f3f73bdb72
suradevv <opapayao1@hotmail.com>
2024-02-23 12:20:43 +0700

    add accessory-type-group , update accessory-type: group_id, vehicle_sub_model: image1,2,3

--- orm/accessory-type.go
+++ orm/accessory-type.go
@@ -2,10 +2,11 @@ package orm
 
 import "github.com/FourWD/middleware/model"
 
-type AccessoryType struct { 
+type AccessoryType struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2); primary_key"`
 	model.GormModel
 
-	Name string `json:"name" query:"name" gorm:"type:varchar(256)"`
+	Name                 string `json:"name" query:"name" gorm:"type:varchar(256)"`
+	AccessoryTypeGroupID string `json:"accessory_type_group_id" query:"accessory_type_group_id" gorm:"type:varchar(36)"`
 	model.GormRowOrder
 }
--- orm/vehicle-sub-model.go
+++ orm/vehicle-sub-model.go
@@ -10,6 +10,9 @@ type VehicleSubModel struct {
 	VehicleModelID string `json:"vehicle_model_id" query:"vehicle_model_id" gorm:"type:varchar(36)"`
 	Name           string `json:"name" query:"name" gorm:"type:varchar(50)"`
 	NameEn         string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
+	Image1         string `json:"image_1" query:"image_1" gorm:"column:image_1;type:varchar(255)"`
+	Image2         string `json:"image_2" query:"image_2" gorm:"column:image_2;type:varchar(255)"`
+	Image3         string `json:"image_3" query:"image_3" gorm:"column:image_3;type:varchar(255)"`
 
 	model.GormRowOrder
 }
