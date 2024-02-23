215fe0a6d859b79fbcd84f90277ad5838ebe72cb
suradevv <opapayao1@hotmail.com>
2024-02-23 12:35:22 +0700

    update middleware

--- orm/accessory-type-group.go
+++ orm/accessory-type-group.go
@@ -5,7 +5,6 @@ import "github.com/FourWD/middleware/model"
 type AccessoryTypeGroup struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2); primary_key"`
 	model.GormModel
-
 	Name string `json:"name" query:"name" gorm:"type:varchar(256)"`
 	model.GormRowOrder
 }
