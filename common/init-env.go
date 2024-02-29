85202f6fe97bd5a6d62041a1c444e89a7c6331a5
puarns <puarns@gmail.com>
2024-02-29 13:43:11 +0700

    no message

--- common/init-env.go
+++ common/init-env.go
@@ -5,25 +5,24 @@ import (
 	"os"
 	"strings"
 
-	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
-func InitEnv() {
+func InitEnv(name string) {
 	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "google.json")
-	viper.SetConfigName("config")
+	viper.SetConfigName(name)
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Error loading config.yaml")
+		fmt.Printf("Error loading %s.yaml", name)
 		panic(err)
 	}
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
-		panic(err)
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	fmt.Println("Error loading .env file")
+	// 	panic(err)
+	// }
 }
