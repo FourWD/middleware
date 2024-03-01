908f2676f92ba36d2b3d6dfcb9d1b6a5047f1dec
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-01 15:31:06 +0700

    Update vehicle-sub-model.go

--- orm/vehicle-sub-model.go
+++ orm/vehicle-sub-model.go
@@ -7,12 +7,13 @@ import (
 type VehicleSubModel struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
-	VehicleModelID string `json:"vehicle_model_id" query:"vehicle_model_id" gorm:"type:varchar(36)"`
-	Name           string `json:"name" query:"name" gorm:"type:varchar(50)"`
-	NameEn         string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
-	Image1         string `json:"image_1" query:"image_1" gorm:"column:image_1;type:varchar(255)"`
-	Image2         string `json:"image_2" query:"image_2" gorm:"column:image_2;type:varchar(255)"`
-	Image3         string `json:"image_3" query:"image_3" gorm:"column:image_3;type:varchar(255)"`
+	VehicleModelID     string `json:"vehicle_model_id" query:"vehicle_model_id" gorm:"type:varchar(36)"`
+	VehicleDriveTypeID string `json:"vehicle_drive_type_id" query:"vehicle_drive_type_id" gorm:"type:varchar(2)"`
+	Name               string `json:"name" query:"name" gorm:"type:varchar(50)"`
+	NameEn             string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
+	Image1             string `json:"image_1" query:"image_1" gorm:"column:image_1;type:varchar(255)"`
+	Image2             string `json:"image_2" query:"image_2" gorm:"column:image_2;type:varchar(255)"`
+	Image3             string `json:"image_3" query:"image_3" gorm:"column:image_3;type:varchar(255)"`
 
 	model.GormRowOrder
 }
