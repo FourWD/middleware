680e8eca3cde81162acd630c86e8590f39e23269
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-01 12:12:45 +0700

    Update vehicle-register-type.go
    add code

--- orm/vehicle-register-type.go
+++ orm/vehicle-register-type.go
@@ -7,6 +7,7 @@ import (
 type VehicleRegisterType struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key"`
 	model.GormModel
+	Code string `json:"code" query:"code" gorm:"type:varchar(10)"`
 	Name string `json:"name" query:"name" gorm:"type:varchar(50)"`
 	model.GormRowOrder
 }
