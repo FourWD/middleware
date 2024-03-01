cff4f009dd4850c2be9af084ff23faab8bee0423
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-01 12:36:01 +0700

    Update company-register-type.go
    add user type id

--- orm/company-register-type.go
+++ orm/company-register-type.go
@@ -8,6 +8,7 @@ type CompanyRegisterType struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key"`
 	model.GormModel
 
-	Name string `json:"name" query:"name" gorm:"type:varchar(100)"`
+	Name       string `json:"name" query:"name" gorm:"type:varchar(100)"`
+	UserTypeID string `json:"user_type_id" query:"user_type_id" gorm:"type:varchar(2)"`
 	model.GormRowOrder
 }
