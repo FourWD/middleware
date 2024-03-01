b1aac8ab8e0dd795df56fe5bb4f6fc2ba5c4c727
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-01 13:10:10 +0700

    Update employee.go
    add emp type id

--- orm/employee.go
+++ orm/employee.go
@@ -9,13 +9,14 @@ type Employee struct {
 
 	Code string `json:"code" query:"code" gorm:"type:varchar(10)"`
 
-	Username     string `json:"username" query:"username" gorm:"type:varchar(20);"`
-	Firstname    string `json:"firstname" query:"firstname" gorm:"type:varchar(100);"`
-	Lastname     string `json:"lastname" query:"lastname" gorm:"type:varchar(20);"`
-	Password     string `json:"password" query:"password" gorm:"type:varchar(36);"`
-	FileAvatarID string `json:"file_avartar_id" query:"file_avartar_id" gorm:"type:varchar(36)"`
-	Mobile       string `json:"mobile" query:"mobile" gorm:"type:varchar(20);"`
-	Email        string `json:"email" query:"email" gorm:"type:varchar(20);"`
-	RunningNo    int    `json:"running_no" query:"running_no" gorm:"type:int;"`
-	IsActive     bool   `json:"is_active" query:"is_active" gorm:"type:bool"`
+	EmployeeTypeID string `json:"employee_type_id" query:"employee_type_id" gorm:"type:varchar(36);"`
+	Username       string `json:"username" query:"username" gorm:"type:varchar(20);"`
+	Firstname      string `json:"firstname" query:"firstname" gorm:"type:varchar(100);"`
+	Lastname       string `json:"lastname" query:"lastname" gorm:"type:varchar(20);"`
+	Password       string `json:"password" query:"password" gorm:"type:varchar(36);"`
+	FileAvatarID   string `json:"file_avartar_id" query:"file_avartar_id" gorm:"type:varchar(36)"`
+	Mobile         string `json:"mobile" query:"mobile" gorm:"type:varchar(20);"`
+	Email          string `json:"email" query:"email" gorm:"type:varchar(20);"`
+	RunningNo      int    `json:"running_no" query:"running_no" gorm:"type:int;"`
+	IsActive       bool   `json:"is_active" query:"is_active" gorm:"type:bool"`
 }
