0cbe20e1b63acd103f37ef6ef536bf635feb7f0f
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-03-01 20:36:26 +0700

    update commit

--- orm/company-vat-type.go
+++ orm/company-vat-type.go
@@ -5,7 +5,7 @@ import (
 )
 
 type CompanyVatType struct {
-	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key"`
+	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key;"`
 	model.GormModel
 
 	Name string `json:"name" query:"name" gorm:"type:varchar(100)"`
--- orm/file.go
+++ orm/file.go
@@ -1,13 +1,12 @@
 package orm
 
 import (
-	"time"
+	"github.com/FourWD/middleware/model"
 )
 
 type File struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
-
-	CreatedAt time.Time `json:"created_at" query:"created_at" gorm:"<-:create"`
+	model.GormModel
 
 	FileName   string `json:"file_name" query:"file_name" gorm:"type:varchar(100)"`
 	Extension  string `json:"extension" query:"extension" gorm:"type:varchar(5)"`
