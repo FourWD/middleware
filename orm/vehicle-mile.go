9b8a5b8d27a849b08dc9462f384cf8c47451255f
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-01 14:49:52 +0700

    Update vehicle-mile.go

--- orm/vehicle-mile.go
+++ orm/vehicle-mile.go
@@ -8,8 +8,10 @@ type VehicleMile struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key"`
 	model.GormModel
 
-	Name    string `json:"name" query:"name" gorm:"type:varchar(20)"`
-	MileMin int    `json:"mile_min" query:"mile_min" gorm:"type:int"`
-	MileMax int    `json:"mile_max" query:"mile_max" gorm:"type:int"`
+	Name      string `json:"name" query:"name" gorm:"type:varchar(20)"`
+	StartMile string `json:"start_mile" query:"start_mile" gorm:"type:varchar(10)"`
+	EndMile   string `json:"end_mile" query:"end_mile" gorm:"type:varchar(10)"`
+	MileMin   int    `json:"mile_min" query:"mile_min" gorm:"type:int"`
+	MileMax   int    `json:"mile_max" query:"mile_max" gorm:"type:int"`
 	model.GormRowOrder
 }
