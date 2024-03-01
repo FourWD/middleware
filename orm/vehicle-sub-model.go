f454a2a0b19b4e1c2bce2b510b0ae4ef3a4e705e
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-01 15:44:37 +0700

    Update vehicle-sub-model.go

--- orm/vehicle-sub-model.go
+++ orm/vehicle-sub-model.go
@@ -10,7 +10,7 @@ type VehicleSubModel struct {
 	VehicleModelID     string `json:"vehicle_model_id" query:"vehicle_model_id" gorm:"type:varchar(36)"`
 	VehicleDriveTypeID string `json:"vehicle_drive_type_id" query:"vehicle_drive_type_id" gorm:"type:varchar(2)"`
 	VehicleGearID      string `json:"vehicle_gear_id" query:"vehicle_gear_id" gorm:"type:varchar(2)"`
-	VehicleFuelTypeID  string `json:"vehicle_fuel_type_id" query:"vehicle_fuel_type_id" gorm:"type:varchar(2)"`
+	VehicleFuelID      string `json:"vehicle_fuel_id" query:"vehicle_fuel_id" gorm:"type:varchar(2)"`
 	Name               string `json:"name" query:"name" gorm:"type:varchar(50)"`
 	NameEn             string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
 	Image1             string `json:"image_1" query:"image_1" gorm:"column:image_1;type:varchar(255)"`
