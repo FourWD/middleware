46c79a10526521ea6926998cfa65167790de7b4f
puarns <puarns@gmail.com>
2024-03-04 15:55:05 +0700

    no message

--- common/encrypt.go
+++ common/encrypt.go
@@ -1,8 +1,17 @@
 package common
 
-import "gorm.io/gorm/clause"
+import (
+	"fmt"
+
+	"gorm.io/gorm/clause"
+)
 
 func EncryptAES(text string) clause.Expr {
 	key := "fourwd"
 	return clause.Expr{SQL: "HEX(AES_ENCRYPT(?, ?))", Vars: []interface{}{text, key}}
+}
+
+func DecryptAESSql(text string) string {
+	key := "fourwd"
+	return fmt.Sprintf(" CAST(AES_DECRYPT(UNHEX(%s), %s) AS CHAR) ", text, key)
 }
