0a58036a8a73c637f12af9dc335b64704c091c27
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-05 22:12:30 +0700

    change tb
    update vehicle maintenance

--- orm/vehicle-maintenance.go
+++ orm/vehicle-maintenance.go
@@ -2,18 +2,29 @@ package orm
 
 import (
 	"time"
-
-	"github.com/FourWD/middleware/model"
 )
 
 type VehicleMaintenance struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
-	model.GormModel
 
-	VehicleID       string    `json:"vehicle_id" query:"vehicle_id" gorm:"type:varchar(36)"`
-	MaintenanceDate time.Time `json:"maintenance_date" query:"maintenance_date"`
-	MaintenanceLocationID string `json:"maintenance_location_id" query:"maintenance_location_id" gorm:"type:varchar(36)"`
-	MaintenanceTypeID     string `json:"maintenance_type_id" query:"maintenance_type_id" gorm:"type:varchar(36)"`
-	Mile                  int    `json:"mile" query:"mile" gorm:"type:int"`
-	Remark                string `json:"remark" query:"remark" gorm:"type:text"`
+	MaintenanceTypeName string    `json:"maintenance_type_name" query:"maintenance_type_name" gorm:"type:varchar(200)"`
+	LocationName        string    `json:"location_name" query:"location_name" gorm:"type:varchar(500)"`
+	ChassisNumber       string    `json:"chassis_number" query:"chassis_number" gorm:"type:varchar(20)"`
+	Mile                int       `json:"mile" query:"mile" gorm:"type:int"`
+	MaintenanceDate     time.Time `json:"maintenance_date" query:"maintenance_date"`
+	Detail              string    `json:"detail" query:"detail" gorm:"type:varchar(500)"`
 }
+
+// type Maintenance struct {
+// 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
+// 	model.GormModel
+
+// 	MaintenanceType     string    `json:"maintenance_type" query:"maintenance_type" gorm:"type:varchar(500)"`
+// 	MaintenanceLocation string    `json:"maintenance_location" query:"maintenance_location" gorm:"type:varchar(500)"`
+// 	ChassisNumber       string    `json:"chassis_number" query:"chassis_number" gorm:"type:varchar(20)"`
+// 	Mile                int       `json:"mile" query:"mile" gorm:"type:int"`
+// 	MaintenanceDate     time.Time `json:"maintenance_date" query:"maintenance_date"`
+// 	Detail              string    `json:"detail" query:"detail" gorm:"type:varchar(500)"`
+
+// 	model.GormRowOrder
+// }
