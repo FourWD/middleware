0b986f26a1a8b8f9e245eb90bba7ac72bad4f712
p-amu <99467582+p-amu@users.noreply.github.com>
2024-03-26 10:48:32 +0700

    Update jwt.go
    return json type

--- common/jwt.go
+++ common/jwt.go
@@ -20,13 +20,13 @@ import (
 
 // JWTClaims struct represents the claims we want to include in the token.
 type JWTClaims struct {
-	UserID string `json:"user_id"`
-	Role   string `json:"role"`
-	Remark string `json:"remark"`
+	UserID string                 `json:"user_id"`
+	Role   string                 `json:"role"`
+	Remark map[string]interface{} `json:"remark"`
 	jwt.StandardClaims
 }
 
-func GenerateJWTToken(userID string, role string, remark string, key []byte, expiresIn time.Duration) (string, error) {
+func GenerateJWTToken(userID string, role string, remark map[string]interface{}, key []byte, expiresIn time.Duration) (string, error) {
 	claims := JWTClaims{
 		UserID: userID,
 		Role:   role,
