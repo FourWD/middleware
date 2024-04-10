8559b87111ff8b3e09aaa2d0a0aefe1cb125c7ab
p-amu <99467582+p-amu@users.noreply.github.com>
2024-04-10 12:14:36 +0700

    role
    role-menu

--- orm/role-menu.go
+++ orm/role-menu.go
@@ -2,11 +2,15 @@ package orm
 
 import "github.com/FourWD/middleware/model"
 
-type RoleTemplate struct {
+type RoleMenu struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
+
 	model.GormModel
+
 	RoleModuleID string `json:"role_module_id" query:"role_module_id" gorm:"type:varchar(36);"`
-	Name         string `json:"name" query:"name" gorm:"type:varchar(100);"`
-	IsActive     bool   `json:"is_active" query:"is_active" gorm:"type:bool;"`
+	RoleMenuID   string `json:"role_menu_id" query:"role_menu_id" gorm:"type:varchar(36);"`
+
+	Name     string `json:"name" query:"name" gorm:"type:varchar(100);"`
+	IsActive bool   `json:"is_active" query:"is_active" gorm:"type:bool;"`
 	model.GormRowOrder
 }
