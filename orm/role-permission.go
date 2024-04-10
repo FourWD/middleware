cf2b9c5728a3098ec3c194d16e21dad4e4e90fd2
p-amu <99467582+p-amu@users.noreply.github.com>
2024-04-10 14:07:51 +0700

    Update role-permission.go

--- orm/role-permission.go
+++ orm/role-permission.go
@@ -5,10 +5,10 @@ import "github.com/FourWD/middleware/model"
 type RolePermission struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
 	model.GormModel
-	RoleID         string `json:"role_id" query:"role_id" gorm:"type:varchar(36); uniqueIndex:idx_role_permissions"`
-	RoleTemplateID string `json:"role_template_id" query:"role_template_id" gorm:"type:varchar(36); uniqueIndex:idx_role_permissions"`
-	IsCreate       bool   `json:"is_create" query:"is_create" gorm:"type:bool;"`
-	IsRead         bool   `json:"is_read" query:"is_read" gorm:"type:bool;"`
-	IsUpdate       bool   `json:"is_update" query:"is_update" gorm:"type:bool;"`
-	IsDelete       bool   `json:"is_delete" query:"is_delete" gorm:"type:bool;"`
+	RoleID     string `json:"role_id" query:"role_id" gorm:"type:varchar(36); uniqueIndex:idx_role_permissions"`
+	RoleMenuID string `json:"role_menu_id" query:"role_menu_id" gorm:"type:varchar(36); uniqueIndex:idx_role_permissions"`
+	IsCreate   bool   `json:"is_create" query:"is_create" gorm:"type:bool;"`
+	IsRead     bool   `json:"is_read" query:"is_read" gorm:"type:bool;"`
+	IsUpdate   bool   `json:"is_update" query:"is_update" gorm:"type:bool;"`
+	IsDelete   bool   `json:"is_delete" query:"is_delete" gorm:"type:bool;"`
 }
