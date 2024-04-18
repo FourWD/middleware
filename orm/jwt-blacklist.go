9a735e82281e49ef05530c2b7d0d6c57234e91ce
p-amu <99467582+p-amu@users.noreply.github.com>
2024-04-18 18:47:06 +0700

    Update jwt-blacklist.go
    md5

--- orm/jwt-blacklist.go
+++ orm/jwt-blacklist.go
@@ -8,5 +8,6 @@ type JwtBlacklist struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
-	Token string `json:"token" query:"token" gorm:"type:text;index"`
+	Md5   string `json:"md5" query:"md5" gorm:"type:varchar(32);index"`
+	Token string `json:"token" query:"token" gorm:"type:text"`
 }
