b1c171c437095600d56a82b183c11cb45a427cfd
puarns <puarns@gmail.com>
2024-04-19 15:41:49 +0700

    no message

--- common/encoded-jwt-token.go
+++ common/encoded-jwt-token.go
@@ -1,75 +1,75 @@
 package common
 
-import (
-	"errors"
-	"fmt"
-	"strings"
-	"time"
+// import (
+// 	"errors"
+// 	"fmt"
+// 	"strings"
+// 	"time"
 
-	"github.com/gofiber/fiber/v2"
-	jwtware "github.com/gofiber/jwt/v2"
-	"github.com/golang-jwt/jwt"
-	"github.com/spf13/viper"
-)
+// 	"github.com/gofiber/fiber/v2"
+// 	jwtware "github.com/gofiber/jwt/v2"
+// 	"github.com/golang-jwt/jwt"
+// 	"github.com/spf13/viper"
+// )
 
-func EncodedJwtToken(c *fiber.Ctx, res string) (string, error) {
-	authorizeToken := c.Get("Authorization")
-	if authorizeToken == "" {
-		return "", errors.New("authorize = nil ")
+// func EncodedJwtToken(c *fiber.Ctx, res string) (string, error) {
+// 	authorizeToken := c.Get("Authorization")
+// 	if authorizeToken == "" {
+// 		return "", errors.New("authorize = nil ")
 
-	}
-	tokenString := strings.Replace(authorizeToken, "Bearer ", "", 1)
-	if tokenString == "" {
-		return "", errors.New("token = nil ")
+// 	}
+// 	tokenString := strings.Replace(authorizeToken, "Bearer ", "", 1)
+// 	if tokenString == "" {
+// 		return "", errors.New("token = nil ")
 
-	}
-	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
+// 	}
+// 	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
 
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return secretKeyToken, nil
-	})
-	if err != nil {
-		return err.Error(), err
-	}
+// 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+// 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+// 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+// 		}
+// 		return secretKeyToken, nil
+// 	})
+// 	if err != nil {
+// 		return err.Error(), err
+// 	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		claimToken := claims[res].(string)
-		return claimToken, nil
-	}
+// 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+// 		claimToken := claims[res].(string)
+// 		return claimToken, nil
+// 	}
 
-	return "", err
-}
+// 	return "", err
+// }
 
-func GenJwtToken(data map[string]interface{}) (string, error) {
-	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
-	// Add additional claims
-	data["expire_date"] = time.Now().Add(time.Minute * 15).Unix()
+// func GenJwtToken(data map[string]interface{}) (string, error) {
+// 	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
+// 	// Add additional claims
+// 	data["expire_date"] = time.Now().Add(time.Minute * 15).Unix()
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(data))
-	tokenString, err := token.SignedString(secretKeyToken)
-	if err != nil {
-		return "", err
-	}
+// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(data))
+// 	tokenString, err := token.SignedString(secretKeyToken)
+// 	if err != nil {
+// 		return "", err
+// 	}
 
-	return tokenString, nil
-}
+// 	return tokenString, nil
+// }
 
-func AuthorizationRequired() fiber.Handler {
-	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
-	return jwtware.New(jwtware.Config{
-		SigningMethod: "HS256",
-		SigningKey:    []byte(secretKeyToken),
-		SuccessHandler: func(c *fiber.Ctx) error {
-			return c.Next()
-		},
-		ErrorHandler: func(c *fiber.Ctx, e error) error {
-			return fiber.ErrUnauthorized
-		},
-	})
-}
+// func AuthorizationRequired() fiber.Handler {
+// 	secretKeyToken := []byte(viper.GetString("jwt_secret_key"))
+// 	return jwtware.New(jwtware.Config{
+// 		SigningMethod: "HS256",
+// 		SigningKey:    []byte(secretKeyToken),
+// 		SuccessHandler: func(c *fiber.Ctx) error {
+// 			return c.Next()
+// 		},
+// 		ErrorHandler: func(c *fiber.Ctx, e error) error {
+// 			return fiber.ErrUnauthorized
+// 		},
+// 	})
+// }
 
 /*
 func main() {
