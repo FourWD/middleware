77ab1759da11738137ffbb82bd19e33b2f67fffb
p-amu <99467582+p-amu@users.noreply.github.com>
2024-04-25 16:51:23 +0700

    Update employee.go
    add passcode

--- orm/employee.go
+++ orm/employee.go
@@ -17,7 +17,7 @@ type Employee struct {
 	Username        string    `json:"username" query:"username" gorm:"type:varchar(20);"`
 	Firstname       string    `json:"firstname" query:"firstname" gorm:"type:varchar(100);"`
 	Lastname        string    `json:"lastname" query:"lastname" gorm:"type:varchar(20);"`
-	Password        string    `json:"password" query:"password" gorm:"type:varchar(64);"`
+	Password        string    `json:"password" query:"password" gorm:"type:varchar(150);"`
 	FileAvatarID    string    `json:"file_avartar_id" query:"file_avartar_id" gorm:"type:varchar(36)"`
 	Mobile          string    `json:"mobile" query:"mobile" gorm:"type:varchar(20);"`
 	Email           string    `json:"email" query:"email" gorm:"type:varchar(20);"`
