ae29ee1892ae701294712e380053032025d74b95
puarns <puarns@gmail.com>
2024-05-08 15:40:56 +0700

    no message

--- common/fiber-table-info.go
+++ common/fiber-table-info.go
@@ -1,45 +1,69 @@
 package common
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type tableInfo struct {
+	TableName   string       `json:"table_name"`
+	IsView      bool         `json:"is_view"`
+	TotalRow    int          `json:"total_row"`
+	TotalColumn int          `json:"total_column"`
+	ColumnList  []columnInfo `json:"column_list,omitempty"` // Use omitempty to skip for views
+}
+
+type columnInfo struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
 
 func FiberTableInfo(app *fiber.App) {
-
-	type ColumnInfo struct {
-		Name string `json:"name"`
-		Type string `json:"type"`
-	}
-
-	type TableInfo struct {
-		TableName   string       `json:"table_name"`
-		TotalColumn int          `json:"total_column"`
-		ColumnList  []ColumnInfo `json:"column_list"`
-	}
-
 	app.Get("/tables", func(c *fiber.Ctx) error {
-		var tables []string
-		if result := Database.Raw("SHOW TABLES").Scan(&tables); result.Error != nil {
+		var tables []struct {
+			TableName string `gorm:"column:Tables_in_auction-dev"`
+			TableType string `gorm:"column:Table_type"`
+		}
+		if result := Database.Raw("SHOW FULL TABLES").Scan(&tables); result.Error != nil {
 			return result.Error
 		}
 
-		var tableInfos []TableInfo
+		var tableInfos []tableInfo
 		for _, table := range tables {
-			var columns []string
-			if result := Database.Raw("SHOW COLUMNS FROM " + table).Scan(&columns); result.Error != nil {
-				return result.Error
+			if table.TableType == "BASE TABLE" { // Check if it's a table
+				tableInfos = append(tableInfos, getTableInfo(Database, table.TableName, false))
 			}
-
-			var columnInfos []ColumnInfo
-			for _, column := range columns {
-				columnInfos = append(columnInfos, ColumnInfo{Name: column})
-			}
-
-			tableInfos = append(tableInfos, TableInfo{
-				TableName:   table,
-				TotalColumn: len(columns),
-				ColumnList:  columnInfos,
-			})
 		}
 
 		return c.JSON(tableInfos)
 	})
+}
+
+func getTableInfo(db *gorm.DB, name string, isView bool) tableInfo {
+	var totalColumns int
+	var columns []struct{ Field, Type string }
+	if !isView {
+		if result := db.Raw("SHOW COLUMNS FROM " + name).Scan(&columns); result.Error != nil {
+			panic(result.Error)
+		}
+		totalColumns = len(columns)
+	}
+
+	var columnInfos []columnInfo
+	for _, column := range columns {
+		columnType := column.Type
+		// Set a default type if column type is empty
+		if columnType == "" {
+			columnType = "UNKNOWN"
+		}
+		columnInfos = append(columnInfos, columnInfo{Name: column.Field, Type: columnType})
+	}
+
+	return tableInfo{
+		TableName:   name,
+		IsView:      isView,
+		TotalRow:    -1, // You need to implement a method to fetch the total rows
+		TotalColumn: totalColumns,
+		ColumnList:  columnInfos,
+	}
 }
