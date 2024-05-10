cb4563feb67dbf5fab9fe4c490f8307b96acd53c
puarns <puarns@gmail.com>
2024-05-10 11:47:16 +0700

    no message

--- common/get-noti-token.go
+++ common/get-noti-token.go
@@ -9,9 +9,16 @@ import (
 func GetNotiToken(c *fiber.Ctx) (string, error) {
 	notiToken, _ := EncodedJwtToken(c, "noti_token") // for old login
 
+	// if notiToken == "" {
+	// 	session := GetSession(c)
+	// 	if value, ok := session.Remark["noti_token"]; ok {
+	// 		return value, nil
+	// 	}
+	// }
+
 	if notiToken == "" {
-		session := GetSession(c)
-		if value, ok := session.Remark["noti_token"]; ok {
+		userClaims := c.Locals("user").(*JWTClaims)
+		if value, ok := userClaims.Remark["noti_token"]; ok {
 			return value, nil
 		}
 	}
