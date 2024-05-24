161bf5a3a6a60091363137635b79c3cc2e20fd74
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-05-24 17:02:04 +0700

    Update notification.go

--- orm/notification.go
+++ orm/notification.go
@@ -13,10 +13,12 @@ type Notification struct {
 	ToUserID              string `json:"to_user_id" query:"to_user_id" gorm:"type:varchar(36)"`
 	ToNotificationTopicID string `json:"to_notification_topic_id" query:"to_notification_topic_id" gorm:"type:varchar(36)"`
 
-	NotificationTypeID string    `json:"notification_type_id" query:"notification_type_id" gorm:"type:varchar(2)"`
-	Message            string    `json:"message" query:"message" gorm:"type:varchar(500)"`
-	Url                string    `json:"url" query:"url" gorm:"type:varchar(500)"`
-	ShowDate           time.Time `json:"show_date" query:"show_date" firestore:"show_date"` 
+	NotificationTypeID string `json:"notification_type_id" query:"notification_type_id" gorm:"type:varchar(2)"`
+	Message            string `json:"message" query:"message" gorm:"type:varchar(500)"`
+	Title              string `json:"title" query:"title" gorm:"type:varchar(500)"`
+
+	Url      string    `json:"url" query:"url" gorm:"type:varchar(500)"`
+	ShowDate time.Time `json:"show_date" query:"show_date" firestore:"show_date"`
 	// IsRead             bool      `json:"is_read" query:"is_read" gorm:"bool"`
 	// ReadDate           time.Time `json:"read_date" query:"read_date"`
 }
