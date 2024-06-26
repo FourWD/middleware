b1ff33fbea15ccb9781529e3cbc425e814bcaa83
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-06-26 10:22:34 +0700

    update middleware

--- common/upload-pdf-to-google.go
+++ common/upload-pdf-to-google.go
@@ -2,13 +2,13 @@ package common
 
 import "github.com/jung-kurt/gofpdf"
 
-func UploadPdfToGoogle(pdf *gofpdf.Fpdf, filename string, appID string, bucket string, auctionID string) (string, error) {
+func UploadPdfToGoogle(pdf *gofpdf.Fpdf, filename string, appID string, bucket string) (string, error) {
 	localPath := "tmp/"
 	if App.GaeService != "" {
 		localPath = "/tmp/"
 	}
 
-	path, err := SavePdf(pdf, filename, localPath, auctionID)
+	path, err := SavePdf(pdf, filename, localPath)
 	if err != nil {
 		return "", err
 	}
@@ -19,3 +19,21 @@ func UploadPdfToGoogle(pdf *gofpdf.Fpdf, filename string, appID string, bucket s
 	}
 	return pathUpload, errUpload
 }
+
+// func UploadPdfToGoogle(pdf *gofpdf.Fpdf, filename string, appID string, bucket string, auctionID string) (string, error) {
+// 	localPath := "tmp/"
+// 	if App.GaeService != "" {
+// 		localPath = "/tmp/"
+// 	}
+
+// 	path, err := SavePdf(pdf, filename, localPath, auctionID)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	pathUpload, errUpload := UploadFileToGoogle(path, "auction", "fourwd-auction")
+// 	if errUpload != nil {
+// 		return "", err
+// 	}
+// 	return pathUpload, errUpload
+// }
