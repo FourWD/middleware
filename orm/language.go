cfe10006eedaf429b45835b05e95510dad6def84
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-07-01 17:49:04 +0700

    update orm

---
+++ orm/language.go
@@ -0,0 +1,16 @@
+package orm
+
+import (
+	"github.com/FourWD/middleware/model"
+)
+
+type Language struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key"`
+	model.GormModel
+
+	Name   string `json:"name" query:"name" gorm:"type:varchar(50)"`
+	NameEn string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
+	Logo   string `json:"logo" query:"logo" gorm:"type:varchar(256)"`
+
+	model.GormRowOrder
+}
