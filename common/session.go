5273613b172678faf5f9960604e0eaed5906f912
PAKKAD\puarns <puarns@gmail.com>
2024-07-26 16:04:22 +0700

    no message

--- common/session.go
+++ common/session.go
@@ -1,13 +1,32 @@
 package common
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// func GetSessionUserID(c *fiber.Ctx) string {
+// 	userClaims := c.Locals("user").(*JWTClaims)
+// 	return userClaims.UserID //
+// }
 
 func GetSessionUserID(c *fiber.Ctx) string {
-	userClaims := c.Locals("user").(*JWTClaims)
-	return userClaims.UserID //
+	userClaims, ok := c.Locals("user").(*JWTClaims)
+	if !ok {
+		log.Printf("Invalid signature [%s]", c.Get("Authorization"))
+		userID, _ := EncodedJwtToken(c, "user_id")
+		return userID
+	}
+
+	return userClaims.UserID
 }
 
 func GetSession(c *fiber.Ctx) *JWTClaims {
-	userClaims := c.Locals("user").(*JWTClaims)
+	userClaims, ok := c.Locals("user").(*JWTClaims)
+	if !ok {
+		log.Printf("Invalid signature [%s]", c.Get("Authorization"))
+	}
+
 	return userClaims
 }
