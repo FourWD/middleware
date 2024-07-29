3136ef9fb25aed6cb7dadf5b56c76ced5cc7c450
PAKKAD\puarns <puarns@gmail.com>
2024-07-29 18:48:57 +0700

    no message

--- common/mongo.go
+++ common/mongo.go
@@ -18,25 +18,43 @@ type MongoDB struct {
 }
 
 func ConnectMongo(key string, databaseName string) {
+	log.Println("ConnectMongo")
 	clientOptions := options.Client().ApplyURI(key)
+	log.Println("a")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+	log.Println("b")
+
+	if clientOptions == nil {
+		log.Fatal("Failed to create client options")
+	}
+
+	if ctx == nil {
+		log.Fatal("Failed to create ctx options")
+	}
 
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Mongo connect: ", err)
+	}
+
+	if client == nil {
+		log.Fatal("Failed to create ctx options")
 	}
 
 	if err = client.Ping(ctx, nil); err != nil {
-		log.Fatal(err)
+		log.Fatal("Mongo ping: ", err)
 	}
 
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
+			log.Fatal("Mongo disconnect: ", err)
 			log.Fatal(err)
 		}
 	}()
 
+	log.Println("c")
+	DatabaseMongo = new(MongoDB)
 	DatabaseMongo.Client = client
 	DatabaseMongo.Database = client.Database(databaseName)
 	DatabaseMongo.Ctx = ctx
