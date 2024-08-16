ecc3be1b41a9156d912e16d97f1a69c5e461542a
PAKKAD\puarns <puarns@gmail.com>
2024-08-16 10:30:49 +0700

    no message

--- common/app.go
+++ common/app.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/FourWD/middleware/model"
 	"github.com/robfig/cron/v3"
-	"github.com/spf13/viper"
 )
 
 func RunLatestVersionOnly() {
@@ -15,33 +14,36 @@ func RunLatestVersionOnly() {
 		return
 	}
 
-	wakeUpUrl := fmt.Sprintf("https://%s-dot-%s.appspot.com/wakeup", App.GaeService, App.GaeProject)
+	handleRunLatestVersionOnly() // run after deploy
+	c := cron.New()
+	c.AddFunc("*/1 * * * *", func() {
+		handleRunLatestVersionOnly()
+	})
+	c.Start()
+}
 
+func handleRunLatestVersionOnly() {
 	type Response struct {
 		Status  int           `json:"status"`
 		Message string        `json:"message"`
 		Data    model.AppInfo `json:"data"`
 	}
 
+	wakeUpUrl := fmt.Sprintf("https://%s-dot-%s.appspot.com/wakeup", App.GaeService, App.GaeProject)
 	var response Response
-	c := cron.New()
-	c.AddFunc("*/1 * * * *", func() {
-		jsonData := CallUrl(wakeUpUrl)
-		if err := json.Unmarshal([]byte(jsonData), &response); err != nil {
-			// fmt.Println("CallWakeUp", err.Error())
-			fmt.Println("************************** Wake up ERROR! **************************")
-			Terminate()
-			return
-		}
+	jsonData := CallUrl(wakeUpUrl)
+	if err := json.Unmarshal([]byte(jsonData), &response); err != nil {
+		// fmt.Println("CallWakeUp", err.Error())
+		fmt.Println("************************** Wake up ERROR! **************************")
+		Terminate()
+		return
+	}
 
-		if response.Status == 1 {
-			if response.Data.AppVersion != viper.GetString("app_version") {
-				Terminate()
-			} else {
-				App.AppVersion = response.Data.AppVersion
-				fmt.Printf("************************** %s [%s] Version: [%s - %s] Wake up OK! **************************\n", App.GaeService, App.Env, App.AppVersion, App.GaeVersion)
-			}
+	if response.Status == 1 {
+		if response.Data.AppVersion != App.AppVersion {
+			Terminate()
+		} else {
+			fmt.Printf("************************** %s [%s] Version: [%s - %s] Wake up OK! **************************\n", App.GaeService, App.Env, App.AppVersion, App.GaeVersion)
 		}
-	})
-	c.Start()
+	}
 }
