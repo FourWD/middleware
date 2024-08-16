091c6151f8e399f019176f55545c0f7209e7f399
PAKKAD\puarns <puarns@gmail.com>
2024-08-16 11:00:10 +0700

    no message

--- common/app.go
+++ common/app.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/FourWD/middleware/model"
 	"github.com/robfig/cron/v3"
+	"github.com/spf13/viper"
 )
 
 func RunLatestVersionOnly() {
@@ -30,8 +31,8 @@ func handleRunLatestVersionOnly() {
 		Data    model.AppInfo `json:"data"`
 	}
 
-	wakeUpUrl := fmt.Sprintf("https://%s-dot-%s.appspot.com/wakeup", App.GaeService, App.GaeProject)
-	log.Println("wakeUpUrl: ", wakeUpUrl, App.AppVersion)
+	wakeUpUrl := fmt.Sprintf("https://%s-dot-%s.appspot.com/wake-up", App.GaeService, App.GaeProject)
+	log.Println("wakeUpUrl: ", wakeUpUrl)
 	var response Response
 	jsonData := CallUrl(wakeUpUrl)
 	if err := json.Unmarshal([]byte(jsonData), &response); err != nil {
@@ -42,7 +43,8 @@ func handleRunLatestVersionOnly() {
 	}
 
 	if response.Status == 1 {
-		if response.Data.AppVersion != App.AppVersion {
+		if response.Data.AppVersion != viper.GetString("app_version") {
+			App.AppVersion = response.Data.AppVersion
 			Terminate()
 		} else {
 			fmt.Printf("************************** %s [%s] Version: [%s - %s] Wake up OK! **************************\n", App.GaeService, App.Env, App.AppVersion, App.GaeVersion)
