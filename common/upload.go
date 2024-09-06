ccf8b1477d390aba7f04c1047ea0caaebde47b3c
p-amu <99467582+p-amu@users.noreply.github.com>
2024-09-06 11:52:16 +0700

    Update upload.go
    print url

--- common/upload.go
+++ common/upload.go
@@ -64,6 +64,7 @@ func uploadFileToServer(p model.UploadPayload, appID string, token string) (mode
 		uploadUrl := "https://pakwan-service.fourwd.me/api/v1/upload/"
 		// uploadUrl := "https://fourwd.as.r.appspot.com/api/v1/upload/"
 		// uploadUrl := "https://pakwan-service.fourwd.me/api/v1/upload/" //
+		Print("pakwan-service", uploadUrl)
 
 		req, err := http.NewRequest("POST", uploadUrl, bytes.NewBuffer(jsonData))
 		if err != nil {
