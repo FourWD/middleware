7df421c36d5c3ea5c4e7f73d440211d4037b624f
p-amu <99467582+p-amu@users.noreply.github.com>
2024-09-11 04:29:14 +0700

    Update upload.go
    fix upload thing

--- common/upload.go
+++ common/upload.go
@@ -71,6 +71,7 @@ func uploadFileToServer(p model.UploadPayload, appID string, token string) (mode
 			fmt.Println(err)
 			return *result, err
 		}
+		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 		req.Header.Add("Content-Type", "application/json")
 		req.Header.Add("Authorization", "Bearer "+token)
 
