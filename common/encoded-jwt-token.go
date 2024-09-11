924a7441df20d9f13012d93a53b846e242ce94c9
PAKKAD\puarns <puarns@gmail.com>
2024-09-11 23:07:17 +0700

    no message

--- common/encoded-jwt-token.go
+++ common/encoded-jwt-token.go
@@ -30,7 +30,7 @@ func EncodedJwtToken(c *fiber.Ctx, res string) (string, error) {
 		return secretKeyToken, nil
 	})
 	if err != nil {
-		return err.Error(), err
+		return "", err
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
