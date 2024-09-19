3faa0297fff2fd0bd12c962b7378d83d4d3057d5
p-amu <99467582+p-amu@users.noreply.github.com>
2024-09-19 19:18:59 +0700

    update filename

--- orm/file.go
+++ orm/file.go
@@ -8,7 +8,7 @@ type File struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
 	model.GormModel
 
-	FileName   string `json:"file_name" query:"file_name" gorm:"type:varchar(500)"`
+	FileName   string `json:"file_name" query:"file_name" gorm:"type:varchar(1024)"`
 	Extension  string `json:"extension" query:"extension" gorm:"type:varchar(5)"`
 	Path       string `json:"path" query:"path" gorm:"type:varchar(500)"`
 	FullPath   string `json:"full_path"  query:"full_path" gorm:"type:varchar(500)"`
