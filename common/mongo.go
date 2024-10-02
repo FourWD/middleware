afd211749ae80a5613b969b386b7cc1c61910c55
PAKKAD\puarns <puarns@gmail.com>
2024-10-02 11:04:28 +0700

    no message

--- common/mongo.go
+++ common/mongo.go
@@ -10,6 +10,7 @@ import (
 )
 
 var DatabaseMongo *MongoDB
+var DatabaseMongoMiddleware *MongoDB
 
 type MongoDB struct {
 	Client   *mongo.Client
@@ -43,6 +44,36 @@ func ConnectMongo(key string, databaseName string) {
 	DatabaseMongo.Client = client
 	DatabaseMongo.Database = client.Database(databaseName)
 	DatabaseMongo.Ctx = ctx
+
+	log.Printf("CONNECT MONGO-DB SUCCESS [%s]", databaseName)
+}
+
+func ConnectMongoMiddleware(key string, databaseName string) {
+	clientOptions := options.Client().ApplyURI(key)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if clientOptions == nil {
+		log.Fatal("Failed to create client options")
+	}
+
+	if ctx == nil {
+		log.Fatal("Failed to create ctx options")
+	}
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("Mongo connect: ", err)
+	}
+
+	if err = client.Ping(ctx, nil); err != nil {
+		log.Fatal("Mongo ping: ", err)
+	}
+
+	DatabaseMongoMiddleware = new(MongoDB)
+	DatabaseMongoMiddleware.Client = client
+	DatabaseMongoMiddleware.Database = client.Database(databaseName)
+	DatabaseMongoMiddleware.Ctx = ctx
 
 	log.Printf("CONNECT MONGO-DB SUCCESS [%s]", databaseName)
 }
