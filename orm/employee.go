ae048b6dcd22cfb313cd05714592302398b834f5
Dave <surasak.sorattayathon@fourwd.me>
2024-10-04 15:15:41 +0700

    update employee email varchar(20) to varchar(50)

--- orm/employee.go
+++ orm/employee.go
@@ -20,7 +20,7 @@ type Employee struct {
 	Password        string    `json:"password" query:"password" gorm:"type:varchar(150);"`
 	FileAvatarID    string    `json:"file_avartar_id" query:"file_avartar_id" gorm:"type:varchar(36)"`
 	Mobile          string    `json:"mobile" query:"mobile" gorm:"type:varchar(20);"`
-	Email           string    `json:"email" query:"email" gorm:"type:varchar(20);"`
+	Email           string    `json:"email" query:"email" gorm:"type:varchar(50);"`
 	RunningNo       int       `json:"running_no" query:"running_no" gorm:"primary_key;auto_increment;not_null"`
 	IsActive        bool      `json:"is_active" query:"is_active" gorm:"type:bool"`
 	CountWrongLogin int       `json:"count_wrong_login" query:"count_wrong_login" gorm:"type:int(1)"`
