bdfce29a9e6db9ac54d7a82f64ad6648b48e56cc
PAKKAD\puarns <puarns@gmail.com>
2024-10-11 14:42:57 +0700

    no message

--- common/logrus.go
+++ common/logrus.go
@@ -2,14 +2,14 @@ package common
 
 import logrus "github.com/sirupsen/logrus"
 
-func LogrusInfo(label string, field logrus.Fields) {
-	field["status"] = 1
-	field["message"] = "success"
-	logrus.WithFields(field).Error(label)
+func LogrusInfo(label string, fields logrus.Fields) {
+	fields["status"] = 1
+	fields["message"] = "success"
+	logrus.WithFields(fields).Error(label)
 }
 
-func LogrusError(label string, field logrus.Fields, err error) {
-	field["status"] = 0
-	field["message"] = err.Error()
-	logrus.WithFields(field).Info(label)
+func LogrusError(label string, fields logrus.Fields, err error) {
+	fields["status"] = 0
+	fields["message"] = err.Error()
+	logrus.WithFields(fields).Info(label)
 }
