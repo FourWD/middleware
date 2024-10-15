cfdf213b9956de3c3e3f571c597cf3d8980183c4
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-10-15 15:30:16 +0700

    Update thai-moblie-number.go

--- common/thai-moblie-number.go
+++ common/thai-moblie-number.go
@@ -2,16 +2,9 @@ package common
 
 import (
 	"regexp"
-	"strings"
 )
 
 func IsThaiMobileNumber(mobile string) bool {
-	mobile = strings.ReplaceAll(mobile, " ", "")
-	mobile = strings.ReplaceAll(mobile, "-", "")
-
-	if strings.HasPrefix(mobile, "+66") {
-		mobile = "0" + mobile[3:]
-	}
 	// Regular expression for Thai mobile number (starts with 06, 08, or 09 and is followed by 8 digits)
 	re := regexp.MustCompile(`^0[689]\d{8}$`)
 	return re.MatchString(mobile)
