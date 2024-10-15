7c134063a7a73609344c5d60e3d580a9b8e3c4bb
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2024-10-15 16:32:43 +0700

    Update thai-moblie-number.go

--- common/thai-moblie-number.go
+++ common/thai-moblie-number.go
@@ -5,7 +5,7 @@ import (
 )
 
 func IsThaiMobileNumber(mobile string) bool {
-	// Regular expression for Thai mobile number (starts with 06, 08, or 09 and is followed by 8 digits)
-	re := regexp.MustCompile(`^0[689]\d{8}$`)
+	// Regular expression for Thai mobile number that starts with +66 or 0
+	re := regexp.MustCompile(`^(\+66|0)[689]\d{8}$`)
 	return re.MatchString(mobile)
 }
