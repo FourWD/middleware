3f5d12a191d20e3b14333010807aa36748ffddd4
PAKKAD\puarns <puarns@gmail.com>
2024-10-16 09:56:52 +0700

    no message

--- common/logrus.go
+++ common/logrus.go
@@ -1,14 +1,20 @@
 package common
 
-import logrus "github.com/sirupsen/logrus"
+import (
+	"time"
+
+	logrus "github.com/sirupsen/logrus"
+)
 
 func LogrusInfo(label string, fields logrus.Fields) {
+	fields["created"] = time.Now().Format(DATE_FORMAT_NANO)
 	fields["status"] = 1
 	fields["message"] = "success"
 	logrus.WithFields(fields).Error(label)
 }
 
 func LogrusError(label string, fields logrus.Fields, err error) {
+	fields["created"] = time.Now().Format(DATE_FORMAT_NANO)
 	fields["status"] = 0
 	fields["message"] = err.Error()
 	logrus.WithFields(fields).Info(label)
