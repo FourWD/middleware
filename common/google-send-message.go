7c2821d2eb56f5091eb3184bb936c397797f55e5
PAKKAD\puarns <puarns@gmail.com>
2025-01-19 11:23:18 +0700

    no message

--- common/google-send-message.go
+++ common/google-send-message.go
@@ -5,14 +5,12 @@ import (
 	"log"
 
 	"cloud.google.com/go/pubsub"
-	"github.com/FourWD/middleware/model"
 	"github.com/spf13/viper"
 )
 
-func GooglePublicMessage(topicName string, gMessage *model.GoogleMessage) (string, error) {
+func GooglePublicMessage(topicName, message string) (string, error) {
 	projectID := viper.GetString("google_project_id")
 
-	message := StructToString(gMessage)
 	log.Println("Message", topicName, message)
 	ctx := context.Background()
 
