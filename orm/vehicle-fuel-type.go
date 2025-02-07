16142993c3ad210c9846bbd6f43c2842f5c316c1
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2025-02-07 15:37:54 +0700

    update orm

--- orm/vehicle-fuel-type.go
+++ orm/vehicle-fuel-type.go
@@ -8,7 +8,7 @@ type VehicleFuelType struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key"`
 	model.GormModel
 
-	Name   string `json:"name" query:"name" gorm:"type:varchar(20)"`
+	Name   string `json:"name" query:"name" gorm:"type:varchar(50)"`
 	NameEn string `json:"name_en" query:"name_en" gorm:"type:varchar(20)"`
 
 	model.GormRowOrder
--- orm/vehicle-sub-type.go
+++ orm/vehicle-sub-type.go
@@ -10,6 +10,7 @@ type VehicleSubType struct {
 
 	VehicleTypeID string `json:"vehicle_type_id" query:"vehicle_type_id" gorm:"type:varchar(10)"`
 	Name          string `json:"name" query:"name" gorm:"type:varchar(100)"`
+	NameEn        string `json:"name_en" query:"name_en" gorm:"type:varchar(100)"`
 
 	model.GormRowOrder
 }
