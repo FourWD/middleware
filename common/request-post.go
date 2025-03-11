41b3dc55ad0729057e2ab15fa544dba3e65cb107
PAKKAD\puarns <puarns@gmail.com>
2025-03-11 13:04:45 +0700

    no message

--- common/request-post.go
+++ common/request-post.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Result struct {
@@ -18,17 +20,25 @@ type Result struct {
 }
 
 func RequestPost(url string, token string, payload map[string]interface{}) (Result, error) {
-	println("token", token)
-	println("url", url)
-	fmt.Printf("payload as struct: %+v\n", payload)
-	payloadJSON, _ := json.MarshalIndent(payload, "", "  ")
-	fmt.Printf("payload as JSON: %s\n", payloadJSON)
+	requestID := uuid.NewString()
+
+	// payloadJSON, _ := json.MarshalIndent(payload, "", "  ")
+
+	logData := map[string]interface{}{
+		"token":   token,
+		"url":     url,
+		"payload": fmt.Sprintf("%+v", payload),
+		// "payload_json": payloadJSON,
+	}
+
+	Log("RequestPost", logData, requestID)
+
 	if token == "" {
+		LogError("no token", nil, requestID)
 		return Result{}, errors.New("no token")
 	}
-	var response Result
-	// baseUrl := viper.GetString("engine_limousine_url")
 
+	var response Result
 	token = strings.ReplaceAll(token, "Bearer ", "")
 	client := http.Client{
 		Timeout: 10 * time.Second,
@@ -38,30 +48,36 @@ func RequestPost(url string, token string, payload map[string]interface{}) (Resu
 	json.NewEncoder(requestPayload).Encode(payload)
 	req, err := http.NewRequest("POST", url, requestPayload)
 	if err != nil {
-		return response, errors.New("req error")
+		LogError(err.Error(), nil, requestID)
+		return response, err
 	}
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+token)
 	responseUrl, err := client.Do(req)
 	if err != nil {
-		return response, errors.New("response error")
+		LogError(err.Error(), nil, requestID)
+		return response, err
 	}
 	defer responseUrl.Body.Close()
 
 	if responseUrl.StatusCode != 200 {
+		LogError("status is not 200", nil, requestID)
 		return response, errors.New("status is not 200")
 	}
 
 	body, err := io.ReadAll(responseUrl.Body)
 	if err != nil {
-		return response, errors.New("body error")
+		LogError(err.Error(), nil, requestID)
+		return response, err
 	}
 
 	if err = json.Unmarshal(body, &response); err != nil {
-
+		LogError(err.Error(), nil, requestID)
+		return response, err
 	}
 
 	if response.Status != 1 {
+		LogError("status error", nil, requestID)
 		return response, errors.New("status error")
 	}
 
