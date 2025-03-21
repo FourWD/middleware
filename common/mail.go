41b2241b719af34f350d5a55ee84ab3c9e04b8a8
PAKKAD\puarns <puarns@gmail.com>
2025-03-21 15:03:37 +0700

    no message

--- common/mail.go
+++ common/mail.go
@@ -9,11 +9,36 @@ import (
 )
 
 func SendMail(sender, subject, body, recipient string) (string, error) {
+	message := mailgun.NewMessage(sender, subject, body, recipient)
+	return sendMail(message)
+}
+
+func SendMailWithTemplate(sender, subject, body, recipient, template string, data map[string]interface{}) (string, error) {
+	message := mailgun.NewMessage(sender, subject, body, recipient)
+	message.SetTemplate(template)
+	addTemplateVariable(message, data)
+
+	return sendMail(message)
+}
+
+func addTemplateVariable(message *mailgun.Message, data map[string]interface{}) {
+	for key, value := range data {
+		strValue, ok := value.(string)
+		if !ok {
+			// log.Printf("Skipping key %s because it's not a string", key)
+			continue
+		}
+		if err := message.AddTemplateVariable(key, strValue); err != nil {
+			// log.Printf("Failed to add template variable: %v", err)
+			continue
+		}
+	}
+}
+
+func sendMail(message *mailgun.Message) (string, error) {
 	domain := viper.GetString("mail.domain")
 	apiKey := viper.GetString("mail.key")
 	mg := mailgun.NewMailgun(domain, apiKey)
-
-	message := mailgun.NewMessage(sender, subject, body, recipient)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
@@ -21,13 +46,13 @@ func SendMail(sender, subject, body, recipient string) (string, error) {
 	_, id, err := mg.Send(ctx, message)
 	// fmt.Printf("ID: %s Resp: %s\n", id, resp)
 
-	logData := map[string]interface{}{
-		"domain":    domain,
-		"sender":    sender,
-		"subject":   subject,
-		"body":      body,
-		"recipient": recipient,
-	}
+	// logData := map[string]interface{}{
+	// 	"sender":    MessageConfig.Sender,
+	// 	"subject":   message.().Subject,
+	// 	"body":      body,
+	// 	"recipient": recipient,
+	// }
+	logData := map[string]interface{}{}
 
 	if err == nil {
 		Log("Send mail", logData, "")
