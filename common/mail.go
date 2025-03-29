9361be4f983f6f572e1a247a9ffb8085f333f50b
p-amu <99467582+p-amu@users.noreply.github.com>
2025-03-30 02:08:23 +0700

    Update mail.go
    add comment attach file to mail

--- common/mail.go
+++ common/mail.go
@@ -63,3 +63,63 @@ func sendMail(message *mailgun.Message) (string, error) {
 
 	return id, err
 }
+
+/* new mailgun attach file
+func SendMailWithTemplate(sender, subject, body, recipient, template string, data map[string]interface{}, atttachURL string, atttachURL2 string) (string, error) {
+	message := mailgun.NewMessage(sender, subject, body, recipient)
+	message.SetTemplate(template)
+	addTemplateVariable(message, data)
+	if atttachURL != "" {
+		// Download the PDF from GCS URL
+		pdfURL := "https://storage.googleapis.com/fourd-aot/receipt/da92fffe-39c7-456d-b9ac-4de20e3eb949.pdf"
+		pdfData, err := downloadFileFromURL(pdfURL)
+		if err != nil {
+			log.Fatalf("Failed to download PDF: %v", err)
+		}
+
+		// Attach PDF file
+		message.AddBufferAttachment("receipt.pdf", pdfData)
+		// if err != nil {
+		// 	log.Fatalf("Failed to attach PDF: %v", err)
+		// }
+		// message.AddAttachment(atttachURL)
+	}
+	if atttachURL2 != "" {
+		// Download the PDF from GCS URL
+		pdfURL := "https://storage.googleapis.com/fourd-aot/receipt/da92fffe-39c7-456d-b9ac-4de20e3eb949.pdf"
+		pdfData, err := downloadFileFromURL(pdfURL)
+		if err != nil {
+			log.Fatalf("Failed to download PDF: %v", err)
+		}
+
+		// Attach PDF file
+		message.AddBufferAttachment("receipt2.pdf", pdfData)
+		// if err != nil {
+		// 	log.Fatalf("Failed to attach PDF: %v", err)
+		// }
+		// message.AddAttachment(atttachURL)
+	}
+
+	return sendMail(message)
+}
+
+func downloadFileFromURL(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to download file: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch file, status: %d", resp.StatusCode)
+	}
+
+	fileData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file content: %v", err)
+	}
+
+	return fileData, nil
+}
+
+*/
