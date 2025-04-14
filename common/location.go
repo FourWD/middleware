55cf65a4c4437edbd69f81cce8d19adcfc8da205
PAKKAD\puarns <puarns@gmail.com>
2025-04-14 13:24:42 +0700

    no message

--- common/location.go
+++ common/location.go
@@ -1,5 +1,6 @@
 package common
 
 func IsInThailand(lat, long float64) bool {
+	// Thailand's approximate latitude and longitude bounds
 	return lat >= 5.6 && lat <= 20.5 && long >= 97.3 && long <= 105.7
 }
