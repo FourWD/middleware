2ba8efbf907e60535cb16bca171b311c64431114
Plew Woo <pew_woothiphom@hotmail.com>
2025-04-30 14:21:21 +0700

    log-action

--- common/create-log-action.go
+++ common/create-log-action.go
@@ -5,11 +5,12 @@ import (
 	"github.com/google/uuid"
 )
 
-func CreateLogAction(userID string, remark string) error {
+func CreateLogAction(userID string, remark string, remarkKey string) error {
 	logAction := new(orm.LogAction)
 	logAction.ID = uuid.NewString()
 	logAction.UserID = userID
 	logAction.Remark = remark
+	logAction.RemarkKey = remarkKey
 
 	return Database.Create(&logAction).Error
 }
--- orm/log-action.go
+++ orm/log-action.go
@@ -6,6 +6,7 @@ type LogAction struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
 	model.GormModel
 
-	UserID string `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
-	Remark string `json:"remark" query:"remark" gorm:"type:varchar(100)"`
+	UserID    string `json:"user_id" query:"user_id" gorm:"type:varchar(36)"`
+	Remark    string `json:"remark" query:"remark" gorm:"type:varchar(100)"`
+	RemarkKey string `json:"remark_key" query:"remark_key" gorm:"type:varchar(100)"`
 }
