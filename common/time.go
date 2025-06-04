646ab6b927cc1f287062d3ff188c0e14bbd4d765
PAKKAD\puarns <puarns@gmail.com>
2025-06-04 10:09:42 +0700

    no message

--- common/time.go
+++ common/time.go
@@ -43,6 +43,12 @@ func CheckInTime(startHour, startMinute, endHour, endMinute int) bool {
 	return now.After(startTime) && now.Before(endTime)
 }
 
+func SameDate(t1, t2 time.Time) bool {
+	y1, m1, d1 := t1.Date()
+	y2, m2, d2 := t2.Date()
+	return y1 == y2 && m1 == m2 && d1 == d2
+}
+
 // func RoundUpToMinute(t time.Time) time.Time {
 // 	rounded := time.Date(
 // 		t.Year(),
