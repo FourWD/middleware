66d5acb0c3956b180d7bd1dc30a323a7985c1443
puarns <puarns@gmail.com>
2025-08-19 18:59:07 +0700

    no message

--- common/rate-limit.go
+++ common/rate-limit.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"sync"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,13 +14,49 @@ func isRateLimitPath(c *fiber.Ctx) bool {
 	return StringExistsInList(c.Path(), publicPaths)
 }
 
-func RateLimit() fiber.Handler {
+var (
+	once        sync.Once
+	rateLimitMW fiber.Handler
+)
+
+func RateLimit(c *fiber.Ctx) error {
+	once.Do(func() {
+		rateLimitMW = limiter.New(limiter.Config{
+			Max:        viper.GetInt("rate_limit_per_second"),
+			Expiration: 1 * time.Second,
+
+			Next: func(c *fiber.Ctx) bool {
+				return !isRateLimitPath(c)
+			},
+
+			KeyGenerator: func(c *fiber.Ctx) string {
+				auth := c.Get("Authorization")
+				if auth == "" {
+					return c.IP()
+				}
+				return auth
+			},
+
+			LimitReached: func(c *fiber.Ctx) error {
+				resp := map[string]interface{}{
+					"status":  0,
+					"message": "rate limit exceeded for this token",
+				}
+				return FiberCustom(c, fiber.StatusTooManyRequests, resp)
+			},
+		})
+	})
+
+	return rateLimitMW(c)
+}
+
+func RateLimit2() fiber.Handler {
 	return limiter.New(limiter.Config{
 		Max:        viper.GetInt("rate_limit_per_second"),
 		Expiration: 1 * time.Second,
 
 		Next: func(c *fiber.Ctx) bool {
-			return isRateLimitPath(c)
+			return !isRateLimitPath(c)
 		},
 
 		KeyGenerator: func(c *fiber.Ctx) string {
