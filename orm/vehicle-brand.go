dd4595d340c71f224ad30ae282890cecc59b85fd
PAKKAD\puarns <puarns@gmail.com>
2025-12-03 01:20:15 +0700

    no message

--- orm/vehicle-brand.go
+++ orm/vehicle-brand.go
@@ -7,13 +7,14 @@ import (
 )
 
 type VehicleBrand struct {
-	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key"`
+	ID string `json:"id" query:"id" gorm:"type:varchar(20);primary_key"`
 	model.GormModel
 
-	Name      string `json:"name" query:"name" gorm:"type:varchar(20)"`
+	Name      string `json:"name" query:"name" gorm:"type:varchar(50)"`
 	NameEn    string `json:"name_en" query:"name_en" gorm:"type:varchar(20)"`
 	ShortName string `json:"short_name" query:"short_name" gorm:"type:varchar(2)"`
 	Logo      string `json:"logo" query:"logo" gorm:"type:varchar(256)"`
+	ModelList string `json:"model_list" query:"model_list" gorm:"column:model_list; type:text"`
 
 	OptionalID1 string `json:"optional_id_1" query:"optional_id_1" gorm:"column:optional_id_1;type:varchar(20)"`
 	OptionalID2 string `json:"optional_id_2" query:"optional_id_2" gorm:"column:optional_id_2;type:varchar(20)"`
